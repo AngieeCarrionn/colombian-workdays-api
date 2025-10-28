c560540d2c3630ccb7b5e83d35f95cda6a53d101
AngieeCarrion <apcnwork@gmail.com>
2025-10-28 15:48:13 -0500

    Se añade middleware para evitar caché en desarrollo

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
 import express, { Application } from "express";
 import cors from "cors";
 import businessDateRouter from "./routes/businessDate.routes";
-import { errorHandler } from "./types/errors"; // importar middleware
+import { errorHandler } from "./types/errors"; // middleware de errores
 
 export const startServer = (): void => {
   const app: Application = express();
 
+  // Habilitar CORS
   app.use(cors());
+
+  // Parseo de JSON
   app.use(express.json());
 
-  // Rutas
+  // Evitar caché en desarrollo para que siempre retorne el código de estado correcto
+  app.use((req, res, next) => {
+    res.setHeader("Cache-Control", "no-store");
+    next();
+  });
+
+  // Rutas de la API
   app.use("/api/business-date", businessDateRouter);
 
   // Middleware de manejo de errores
